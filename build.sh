45556e780fa2f02e06bf4041b1a5e35a94d67bcb
Stephan Raabe <mail@stephan-raabe.de>
2024-08-04 16:34:12 +0200

    Updates

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ..
-ARCH=x86_64 appimagetool ml4w-dotfiles-uninstaller
+ARCH=x86_64 appimagetool dotfiles-uninstaller
 echo ":: AppImage created"
 cp ML4W_Dotfiles_Uninstaller-x86_64.AppImage ~/dotfiles-versions/dotfiles/dotfiles/.config/ml4w/apps/ML4W_Dotfiles_Uninstaller.AppImage
 echo ":: AppImage copied to ~/dotfiles-versions/dotfiles/dotfiles/.config/ml4w/apps"
